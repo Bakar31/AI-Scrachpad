c3cb4fce2de8418fe481da3eda7c725bce1f3f77
Bakar31 <abubakar1808031@gmail.com>
2023-04-12 12:32:46 +0600

    alexnet pytorch

--- Model Implementation/Pytorch/alexnet.py
+++ Model Implementation/Pytorch/alexnet.py
@@ -1,27 +1,16 @@
-import tensorflow as tf
+import torch
+from torch import nn
+import torch.functional as F
 
-input_height = 224
-input_width = 224
-
-# model implementation
-model = tf.keras.models.Sequential(
-    [
-        tf.keras.Input((input_height, input_width, 3)),
-        tf.keras.layers.Conv2D(96, (11, 11), 4, activation= 'relu'),
-        tf.keras.layers.MaxPool2D((3, 3), strides = (2, 2)),
-        tf.keras.layers.Conv2D(256, (5, 5), padding = 'same', activation = 'relu'),
-        tf.keras.layers.MaxPool2D((3, 3), strides = (2, 2)),
-        tf.keras.layers.Conv2D(384, (3, 3), padding= 'same' , activation = 'relu'),
-        tf.keras.layers.Conv2D(384, (3, 3), padding= 'same', activation = 'relu'),
-        tf.keras.layers.Conv2D(256, (3, 3), padding= 'same', activation = 'relu'),
-        tf.keras.layers.MaxPool2D((3, 3), (2, 2)),
-        tf.keras.layers.Flatten(),
-        tf.keras.layers.Dense(4096,activation= 'relu'),
-        tf.keras.layers.Dense(4096,activation= 'relu'),
-        tf.keras.layers.Dense(1000, activation= 'softmax')
+class AlexNet(nn.Module):
+    def __init__(self) -> None:
+        super().__init__()
         
-    ]
-)
-
-model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
-print(model.summary())
+        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=1)
+        self.conv2 = nn.Conv2d(96, 256, 5, 1, 3)
+        self.conv3 = nn.Conv2d(256, 384, 3, 1, 1)
+        self.conv4 = nn.Conv2d(384, 384, 3, 1, 1)
+        self.conv3 = nn.Conv2d(384, 256, 3, 1, 1)
+        self.maxpool3 = nn.MaxPool2d(3, 2)
+        self.fc1 = 
+    
