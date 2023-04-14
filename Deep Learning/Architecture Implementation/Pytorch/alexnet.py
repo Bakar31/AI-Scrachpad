fe1a2ad41ce324fc3c66e7e94cd55e398c2fd82d
Bakar31 <abubakar1808031@gmail.com>
2023-04-14 13:11:53 +0600

    alexnet pytorch implementation

--- Deep Learning/Architecture Implementation/Pytorch/alexnet.py
+++ Deep Learning/Architecture Implementation/Pytorch/alexnet.py
@@ -1,16 +1,38 @@
 import torch
 from torch import nn
-import torch.functional as F
 
 class AlexNet(nn.Module):
     def __init__(self) -> None:
         super().__init__()
         
-        self.conv1 = nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=1)
-        self.conv2 = nn.Conv2d(96, 256, 5, 1, 3)
-        self.conv3 = nn.Conv2d(256, 384, 3, 1, 1)
-        self.conv4 = nn.Conv2d(384, 384, 3, 1, 1)
-        self.conv3 = nn.Conv2d(384, 256, 3, 1, 1)
-        self.maxpool3 = nn.MaxPool2d(3, 2)
-        self.fc1 = 
-    
+        self.features = nn.Sequential(
+            nn.Conv2d(in_channels=3, out_channels=96, kernel_size=11, stride=4, padding=1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(96, 256, 5, 1, 3),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2),
+            nn.Conv2d(256, 384, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 384, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.Conv2d(384, 256, 3, 1, 1),
+            nn.ReLU(inplace=True),
+            nn.MaxPool2d(3, 2)
+        )
+
+        self.classifier = nn.Sequential(
+            nn.Dropout(0.5),
+            nn.Linear(256 * 6 * 6, 4096),
+            nn.ReLU(inplace=True),
+            nn.Dropout(0.5),
+            nn.Linear(4096, 4096),
+            nn.ReLU(inplace=True),
+            nn.Linear(4096, 1000)
+        )
+
+    def forward(self, x):
+        x = self.features(x)
+        x = torch.flatten(x, 1)
+        x = self.classifier(x)
+        return x
--- Deep Learning/Architecture Implementation/Tensorflow/alexnet.py
+++ Deep Learning/Architecture Implementation/Tensorflow/alexnet.py
@@ -16,7 +16,9 @@ model = tf.keras.models.Sequential(
         tf.keras.layers.Conv2D(256, (3, 3), padding= 'same', activation = 'relu'),
         tf.keras.layers.MaxPool2D((3, 3), (2, 2)),
         tf.keras.layers.Flatten(),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(4096,activation= 'relu'),
+        tf.keras.layers.Dropout(0.5),
         tf.keras.layers.Dense(4096,activation= 'relu'),
         tf.keras.layers.Dense(1000, activation= 'softmax')
         
